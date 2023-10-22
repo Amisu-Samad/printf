009382fa40769858c048fd2f9e7c0fb282b77d1e
Amisu-Samad <adeboladamilare70@gmail.com>
2023-10-22 03:23:11 -0700

    My first printf function

--- print_write_handlers.c
+++ print_write_handlers.c
@@ -114,7 +114,8 @@ int write_num(int ind, char buffer[],
 		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 		{
 			if (extra_c)
-				buffer[--ind] = extra_c;			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 		{
